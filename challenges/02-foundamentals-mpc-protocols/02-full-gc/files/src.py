e2ccd161d10df85f1cbecca41f72745382c22ab5
Lorenzo Bunaj <lorenzobunaj@gmail.com>
2025-07-08 17:38:22 +0100

    challenge 02/02: solver

--- challenges/02-foundamentals-mpc-protocols/02-full-gc/files/src.py
+++ challenges/02-foundamentals-mpc-protocols/02-full-gc/files/src.py
@@ -1,28 +1,37 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
+from random import randint
 from utils import xor, H
 
-with open("flag.txt") as f:
-    FLAG = f.read().strip()
+# with open("flag.txt") as f:
+#     FLAG_CONTENT = f.read().strip()
 
+FLAG_CONTENT = "JN3Uy7ubhJwN48239CN"
+FLAG = "RHUL{" + FLAG_CONTENT + "}"
 secret = get_random_bytes(16)
 iv = get_random_bytes(16)
 
 cipher = AES.new(secret, AES.MODE_CBC, iv)
-ct = cipher.encrypt(pad(FLAG.encode(), AES.block_size))
+ct = cipher.encrypt(pad(FLAG_CONTENT.encode(), AES.block_size))
 
-operations = ['AND', 'NOR']
+operations = ['AND', 'NOR', 'NAND', 'XOR']
 def compute(a, b, op):
+    res = None
     if op == 'AND':
-        return a * b
+        res = a & b
     elif op == 'NOR':
-        return (1-a) * (1-b)
+        res = not (a | b)
+    elif op == 'NAND':
+        res = not (a & b)
+    elif op == 'XOR':
+        res = a ^ b
+
+    return int(res)
 
 def garble_gate(Wa, Wb, x, y):
-    op = operations[(x + y) % 2]
-
     Lc = [get_random_bytes(32), bytes([0] * 31 + [secret[4 * x + y]])]
+    op = operations[(x + y) % 4]
 
     table = [None] * 4
     for a in [0, 1]:
@@ -32,39 +41,39 @@ def garble_gate(Wa, Wb, x, y):
 
             pc = compute(pa, pb, op)
             ct = xor(H(La, Lb), Lc[pc])
-            table[2*a + b] = (ct, pc)
+        
+            table[2 * pa + pb] = ct
 
-    return table
+    return Lc, table
 
-def garble_circuit(wires):
+def garble_circuit(ws):
+    wires = ws
     gates = []
 
+    new_wires = None
     for x in range(4):
+        gates.append([])
+        new_wires = []
         for y in range(4):
-            gates.append(garble_gate(wires[y], wires[(y+1) % 4], x, y))
+            Lc, table = garble_gate(wires[y], wires[(y+1) % 4], x, y)
 
-        for y in range(4):
-            w0 = None
-            w1 = None
-            for ct, p in gates[4 * x + y]:
-                if w0 is not None and w1 is not None:
-                    break
-                if p == 0 and w0 is None:
-                    w0 = (ct, 0)
-                elif p == 1 and w1 is None:
-                    w1 = (ct, 1)
-            
-            wires[y] = [w0, w1]
+            gates[x].append(table)
+            new_wires.append([
+                (Lc[0], 0),
+                (Lc[1], 1)
+            ])
+
+        wires = [w for w in new_wires]
 
     return gates
-        
 
 def protocol():
     wires = []
     for _ in range(4):
+        b = randint(0,1)
         wires.append([
-            (get_random_bytes(32), 0),
-            (get_random_bytes(32), 1)
+            (get_random_bytes(32), b),
+            (get_random_bytes(32), 1-b)
         ])
     
     gates = garble_circuit(wires)
