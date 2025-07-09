610de95e339997f8f750219fdb606e423ede1a71
Lorenzo Bunaj <lorenzobunaj@gmail.com>
2025-07-09 16:11:29 +0100

    improved and better modularity in the docker infrastructure

--- challenges/01-defining-mpc/00-semihonest-security/run.sh
+++ challenges/01-defining-mpc/00-semihonest-security/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# start the Python server in the background
 python3 ./files/server.py
+
+# forward 0.0.0.0:1337 to 127.0.0.1:1337
+# exec socat TCP-LISTEN:1337,reuseaddr,fork TCP:127.0.0.1:1337
